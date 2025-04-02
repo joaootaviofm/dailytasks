b98945b7e0b35b8d4897d00d6a937bb5a74ddbd2
joaootaviofm <joaootaviofm@gmail.com>
2025-04-02 10:18:48 -0300

    feat: added background to main div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./_components/Navbar"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-center bg-[url('/background.jpg')] min-w-screen min-h-screen overflow-x-hidden">
       <Navbar/>
     </div>
   )
