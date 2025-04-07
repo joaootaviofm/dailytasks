d4719ab96bdf0ae10c3ae637d1a32244bbfa889b
joaootaviofm <joaootaviofm@gmail.com>
2025-04-07 14:33:09 -0300

    feat: added task creation page

---
+++ src/app/create/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../_components/home/Navbar";
+
+export default function Create(){
+    return (
+        <div className="bg-center bg-[url('/background.jpg')] min-w-screen min-h-screen overflow-x-hidden">
+            <Navbar/>
+        </div>
+    )
+}
