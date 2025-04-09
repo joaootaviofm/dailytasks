f88b335fb3e3df33540dfbb5cad6eeeed12cd8a0
joaootaviofm <joaootaviofm@gmail.com>
2025-04-09 09:38:49 -0300

    feat: added option to delete task

--- src/app/tasks/TaskHero.tsx
+++ src/app/tasks/TaskHero.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 type Task = {
-  id: number;
+  id: string;
   task: string;
   description: string;
 };
@@ -22,9 +22,20 @@ export default function TaskHero() {
         "http://localhost:8080/tasks",
         taskAdded
       );
+      setTasks((prev) => [...prev, response.data])
+      
       console.log(response.data);
     } catch (error) {
       console.error("Error adding task.", error);
+    }
+  }
+
+  async function handleDelete(id: string) {
+    try {
+      await axios.delete(`http://localhost:8080/tasks/delete/${id}`);
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    } catch (error) {
+      console.error("Wasn't able to delete that!", error);
     }
   }
 
@@ -41,7 +52,7 @@ export default function TaskHero() {
 
   return (
     <div className="mt-[200px] flex items-center justify-center">
-      <div className="max-w-[1000px] bg-white/20 rounded-lg backdrop-blur-md border border-black p-[20px] flex flex-col gap-[10px] mb-[20px]">
+      <div className="min-w-[500px] max-w-[1000px] bg-white/20 rounded-lg backdrop-blur-md border border-black p-[30px] flex flex-col gap-[10px] mb-[20px]">
         <h1 className="font-bold text-2xl text-center mb-[10px] text-transparent bg-clip-text bg-gradient-to-r from-[#9C83FF] to-[#FF9051]">
           {tasks.length == 0 ? "You have no tasks!" : "Just do it."}
         </h1>
@@ -81,7 +92,10 @@ export default function TaskHero() {
                 <button className="hover:text-orange-400 bg-white/20 hover:bg-black/20 duration-300 rounded-md font-bold text-white cursor-pointer px-2 py-1">
                   Edit
                 </button>
-                <button className="hover:text-red-500 bg-white/20 hover:bg-black/20 duration-300 rounded-md font-bold text-white cursor-pointer px-2 py-1">
+                <button
+                  onClick={() => handleDelete(item.id)}
+                  className="hover:text-red-500 bg-white/20 hover:bg-black/20 duration-300 rounded-md font-bold text-white cursor-pointer px-2 py-1"
+                >
                   Delete
                 </button>
               </div>
